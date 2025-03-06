6bb7b14a67fea5baadc61a8a80318f88637faed3
Vinícius Castilho <viniciuscastilho8@gmail.com>
2025-03-06 20:14:19 -0300

    Update AtividadeAula2802.java

--- AtividadeAula2802.java
+++ AtividadeAula2802.java
@@ -14,6 +14,10 @@ class Node<T> {
         return value;
     }
 
+    public void setValue(T value) {
+        this.value = value;
+    }
+
     public Node<T> getNext() {
         return next;
     }
@@ -35,6 +39,14 @@ class UniBHList<T extends Comparable<T>> {
         totalElements++;
     }
 
+    public Node<T> removeAtBeginning() {
+        if (firstNode == null) return null;
+        Node<T> aux = firstNode;
+        firstNode = firstNode.getNext();
+        totalElements--;
+        return aux;
+    }
+
     public void insertAtEnd(T value) {
         Node<T> newNode = new Node<>(value);
         if (firstNode == null) {
@@ -49,77 +61,51 @@ class UniBHList<T extends Comparable<T>> {
         totalElements++;
     }
 
-    public void insertInOrder(T value) {
-        Node<T> newNode = new Node<>(value);
-        if (firstNode == null || firstNode.getValue().compareTo(value) > 0) {
-            newNode.setNext(firstNode);
-            firstNode = newNode;
-        } else {
-            Node<T> current = firstNode;
-            while (current.getNext() != null && current.getNext().getValue().compareTo(value) < 0) {
-                current = current.getNext();
-            }
-            newNode.setNext(current.getNext());
-            current.setNext(newNode);
+    public void insertAfter(int index, T value) {
+        if (index < 0 || index >= totalElements) {
+            throw new IndexOutOfBoundsException("Índice inválido");
         }
+        Node<T> current = firstNode;
+        for (int i = 0; i < index; i++) {
+            current = current.getNext();
+        }
+        Node<T> newNode = new Node<>(value);
+        newNode.setNext(current.getNext());
+        current.setNext(newNode);
         totalElements++;
     }
 
-    public Node<T> removeAtBeginning() {
-        if (firstNode == null) return null;
-        Node<T> aux = firstNode;
-        firstNode = firstNode.getNext();
-        totalElements--;
-        return aux;
-    }
-
-    public Node<T> removeAtEnd() {
-        if (firstNode == null) return null;
-        if (firstNode.getNext() == null) {
-            Node<T> aux = firstNode;
-            firstNode = null;
-            totalElements--;
-            return aux;
+    public Node<T> removeAt(int index) {
+        if (index < 0 || index >= totalElements) {
+            throw new IndexOutOfBoundsException("Índice inválido");
         }
-
+        if (index == 0) {
+            return removeAtBeginning();
+        }
         Node<T> current = firstNode;
-        while (current.getNext().getNext() != null) {
+        for (int i = 0; i < index - 1; i++) {
             current = current.getNext();
         }
-        Node<T> aux = current.getNext();
-        current.setNext(null);
+        Node<T> removedNode = current.getNext();
+        current.setNext(removedNode.getNext());
         totalElements--;
-        return aux;
+        return removedNode;
     }
 
-    public boolean removeByValue(T value) {
-        if (firstNode == null) return false;
-        if (firstNode.getValue().equals(value)) {
-            firstNode = firstNode.getNext();
-            totalElements--;
-            return true;
-        }
-
-        Node<T> current = firstNode;
-        while (current.getNext() != null && !current.getNext().getValue().equals(value)) {
-            current = current.getNext();
-        }
-
-        if (current.getNext() == null) return false;
-        current.setNext(current.getNext().getNext());
-        totalElements--;
-        return true;
+    public int size() {
+        return totalElements;
     }
 
-    public Node<T> search(T value) {
+    public void updateElement(T oldValue, T newValue) {
         Node<T> current = firstNode;
         while (current != null) {
-            if (current.getValue().equals(value)) {
-                return current;
+            if (current.getValue().equals(oldValue)) {
+                current.setValue(newValue);
+                return;
             }
             current = current.getNext();
         }
-        return null;
+        throw new IllegalArgumentException("Elemento não encontrado na lista");
     }
 
     @Override
@@ -152,88 +138,72 @@ public class AtividadeAula2802 {
         int option;
 
         do {
-            System.out.println("\n=== Menu da Lista Encadeada ===");
+            System.out.println("\n=== Menu da Lista ===");
             System.out.println("1 - Inserir no início");
             System.out.println("2 - Inserir no final");
-            System.out.println("3 - Inserir em ordem");
-            System.out.println("4 - Remover do início");
-            System.out.println("5 - Remover do final");
-            System.out.println("6 - Remover por valor");
-            System.out.println("7 - Buscar elemento");
-            System.out.println("8 - Exibir lista");
+            System.out.println("3 - Inserir após índice");
+            System.out.println("4 - Remover por índice");
+            System.out.println("5 - Retornar tamanho da lista");
+            System.out.println("6 - Modificar elemento");
+            System.out.println("7 - Exibir lista");
             System.out.println("0 - Sair");
             System.out.print("Escolha uma opção: ");
             
             option = scanner.nextInt();
 
-            switch (option) {
-                case 1:
-                    System.out.print("Digite um número para inserir no início: ");
-                    int num1 = scanner.nextInt();
-                    list.insertAtBeginning(num1);
-                    System.out.println("Elemento adicionado! Lista atual: " + list);
-                    break;
+            try {
+                switch (option) {
+                    case 1:
+                        System.out.print("Digite um número para inserir no início: ");
+                        int num1 = scanner.nextInt();
+                        list.insertAtBeginning(num1);
+                        break;
 
-                case 2:
-                    System.out.print("Digite um número para inserir no final: ");
-                    int num2 = scanner.nextInt();
-                    list.insertAtEnd(num2);
-                    System.out.println("Elemento adicionado! Lista atual: " + list);
-                    break;
+                    case 2:
+                        System.out.print("Digite um número para inserir no final: ");
+                        int num2 = scanner.nextInt();
+                        list.insertAtEnd(num2);
+                        break;
+                    
+                    case 3:
+                        System.out.print("Digite o índice onde deseja inserir: ");
+                        int index = scanner.nextInt();
+                        System.out.print("Digite o valor: ");
+                        int value = scanner.nextInt();
+                        list.insertAfter(index, value);
+                        break;
+                    
+                    case 4:
+                        System.out.print("Digite o índice do elemento a ser removido: ");
+                        int removeIndex = scanner.nextInt();
+                        list.removeAt(removeIndex);
+                        break;
+                    
+                    case 5:
+                        System.out.println("Tamanho da lista: " + list.size());
+                        break;
+                    
+                    case 6:
+                        System.out.print("Digite o valor a ser modificado: ");
+                        int oldValue = scanner.nextInt();
+                        System.out.print("Digite o novo valor: ");
+                        int newValue = scanner.nextInt();
+                        list.updateElement(oldValue, newValue);
+                        break;
+                    
+                    case 7:
+                        System.out.println("Lista atual: " + list);
+                        break;
 
-                case 3:
-                    System.out.print("Digite um número para inserir em ordem: ");
-                    int num3 = scanner.nextInt();
-                    list.insertInOrder(num3);
-                    System.out.println("Elemento adicionado! Lista atual: " + list);
-                    break;
+                    case 0:
+                        System.out.println("Saindo do programa...");
+                        break;
 
-                case 4:
-                    if (list.removeAtBeginning() != null) {
-                        System.out.println("Elemento removido do início! Lista atual: " + list);
-                    } else {
-                        System.out.println("A lista está vazia!");
-                    }
-                    break;
-
-                case 5:
-                    if (list.removeAtEnd() != null) {
-                        System.out.println("Elemento removido do final! Lista atual: " + list);
-                    } else {
-                        System.out.println("A lista está vazia!");
-                    }
-                    break;
-
-                case 6:
-                    System.out.print("Digite o valor a ser removido: ");
-                    int valueToRemove = scanner.nextInt();
-                    if (list.removeByValue(valueToRemove)) {
-                        System.out.println("Elemento removido! Lista atual: " + list);
-                    } else {
-                        System.out.println("Elemento não encontrado!");
-                    }
-                    break;
-
-                case 7:
-                    System.out.print("Digite o valor a ser buscado: ");
-                    int searchValue = scanner.nextInt();
-                    if (list.search(searchValue) != null) {
-                        System.out.println("Elemento encontrado na lista!");
-                    } else {
-                        System.out.println("Elemento não encontrado!");
-                    }
-                    break;
-
-                case 8:
-                    System.out.println("Lista atual: " + list);
-                    break;
-
-                case 0:
-                    System.out.println("Saindo do programa...");
-                    break;
-
-                default:
-                    System.out.println("Opção inválida! Tente novamente.");
+                    default:
+                        System.out.println("Opção inválida! Tente novamente.");
+                }
+            } catch (Exception e) {
+                System.out.println("Erro: " + e.getMessage());
             }
         } while (option != 0);
 
